12965a010538e774d8cfc31b8abe3fe5ec5aafdc
Mike Taves <mwtoews@gmail.com>
2022-05-04 23:26:54 +1200

    Don't fail test script if speedups are not available

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,10 +1,15 @@
+"""Run test script for shapely."""
 import platform
 import py
 import os
 import sys
 
+from shapely import speedups
+from shapely.geometry import LineString
+
+
 implementation = platform.python_implementation()
-print('implementation: {}'.format(implementation))
+print(f'implementation: {implementation}')
 target_platform = os.environ["target_platform"]
 print(f'target platform: {target_platform}')
 py_version = sys.version_info[:2]
@@ -12,20 +17,17 @@ print(f'python version: {py_version}')
 
 pytest_args = ['tests']
 
-if implementation != 'PyPy':
-    from shapely import speedups
-    import shapely.speedups._speedups
-    import shapely.vectorized
-    import shapely.vectorized._vectorized
 
-    assert speedups.available;
-
+if speedups.available and not speedups.enabled:
     speedups.enable()
+
+print("speedups enabled: {speedups.enabled}")
+if speedups.enabled:
     pytest_args.append('--with-speedups')
+else:
+    pytest_args.append('--without-speedups')
 
 py.test.cmdline.main(pytest_args)
-
-from shapely.geometry import LineString
 
 ls = LineString([(0, 0), (10, 0)])
 # On OSX causes an abort trap, due to https://github.com/Toblerity/Shapely/issues/177
